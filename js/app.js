d457c5093bfbbcd460d1fc77bd1ec705416a4108
Helmatron <ema.matro@gmail.com>
2024-03-08 08:29:12 +0100

    stamp: prezzo + ridotto20 + ridotto40

--- js/app.js
+++ js/app.js
@@ -21,17 +21,26 @@ console.log(età);
 Quindi: km x 0.21€
 */
 
-let prezzo = km * 0.21 + '€';
-console.log(prezzo);
+let prezzo = km * 0.21;
+let prezzod = prezzo.toFixed(2);
+console.log(prezzod + '€'); //string
+
 
 
 /*2- Applicare sconto del 20% per i minorenni*/
 
-if(età < 18){
-    let ridotto20 = prezzo - 20%
-    console.log(ridotto20);
+if(età <= 18) {
+    let ridotto20 = prezzo - (prezzo * 20 / 100)
+    let ridotto20d = ridotto20.toFixed(2);
+    console.log(ridotto20d + '€'); //string
 }
 
 /*3- Applicare sconto del 40% per gli over 65*/
+
+if(età >= 65) {
+    let ridotto40 = prezzo - (prezzo * 40 / 100)
+    let ridotto40d = ridotto40.toFixed(2);
+    console.log(ridotto40d + '€'); //string
+}
 
 /*STAMPARE il prezzo finale del biglietto con massimo due decimali per i centesimi*/
